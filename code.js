45ce743bf75876930d56a1117d3d0743f7b3e712
irinapech <120232888+irinapech@users.noreply.github.com>
2023-07-21 15:33:54 -0500

    Fix the button position

--- code.js
+++ code.js
@@ -30,47 +30,85 @@ function draw() {
     }
 }
 
-let minutes = .1;
-let seconds = minutes * 60;
-function countdown() {
-    setTimeout('Decrement()', 60);
-}
-
-function Decrement() {
-    if (document.getElementById) {
-        minutes = document.getElementById("minutes");
-        seconds = document.getElementById("seconds");
-        if (seconds < 59) {
-            seconds.value = seconds;
-        }
-        else {
-            minutes.value = getminutes();
-            seconds.value = getseconds();
-        }
-        if (minutes < 1) {
-            minutes.style.color = "red";
-            seconds.style.color = "red";
-        }
-        if (minutes < 0) {
-            alert('time up');
-            minutes.value = 0;
-            seconds.value = 0;
-        }
-        else {
-            seconds--;
-            setTimeout('Decrement()', 1000);
-        }
-    }
-}
-
-function getminutes() {
-    minutes = Math.floor(seconds / 60);
-    return minutes;
-}
-
-function getseconds() {
-    return seconds - Math.round(minutes * 60);
-}
-
 btn.addEventListener('click', draw);
-btn.addEventListener('click', countdown);
+
+class Timer {
+    constructor(root) {
+      root.innerHTML = Timer.getHTML();
+  
+      this.el = {
+        minutes: root.querySelector(".timer__part--minutes"),
+        seconds: root.querySelector(".timer__part--seconds"),
+        control: root.querySelector(".timer__btn--control"),
+        reset: root.querySelector(".timer__btn--reset")
+      };
+  
+      this.interval = null;
+      this.remainingSeconds = 0;
+  
+      this.el.control.addEventListener("click", () => {
+        if (this.interval === null) {
+          this.start();
+        } else {
+          this.stop();
+        }
+      });
+  
+      this.el.reset.addEventListener("click", () => {
+        const inputMinutes = prompt("Enter number of minutes:");
+  
+        if (inputMinutes < 60) {
+          this.stop();
+          this.remainingSeconds = inputMinutes * 60;
+          this.updateInterfaceTime();
+        }
+      });
+    }
+  
+    updateInterfaceTime() {
+      const minutes = Math.floor(this.remainingSeconds / 60);
+      const seconds = this.remainingSeconds % 60;
+  
+      this.el.minutes.textContent = minutes.toString().padStart(2, "0");
+      this.el.seconds.textContent = seconds.toString().padStart(2, "0");
+    }
+  
+    updateInterfaceControls() {
+      if (this.interval === null) {
+        this.el.control.innerHTML = `<span class="material-icons">play_arrow</span>`;
+        this.el.control.classList.add("timer__btn--start");
+        this.el.control.classList.remove("timer__btn--stop");
+      } else {
+        this.el.control.innerHTML = `<span class="material-icons">pause</span>`;
+        this.el.control.classList.add("timer__btn--stop");
+        this.el.control.classList.remove("timer__btn--start");
+      }
+    }
+  
+    start() {
+      if (this.remainingSeconds === 0) return;
+  
+      this.interval = setInterval(() => {
+        this.remainingSeconds--;
+        this.updateInterfaceTime();
+  
+        if (this.remainingSeconds === 0) {
+          this.stop();
+        }
+      }, 1000);
+  
+      this.updateInterfaceControls();
+    }
+  
+    stop() {
+      clearInterval(this.interval);
+  
+      this.interval = null;
+  
+      this.updateInterfaceControls();
+    }
+  }
+  
+  new Timer(
+      document.querySelector(".timer")
+  );
