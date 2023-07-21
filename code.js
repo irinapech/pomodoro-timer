96b58dcb35e8c096627c61ab9f33bf4a0b25d5b9
irinapech <120232888+irinapech@users.noreply.github.com>
2023-07-21 11:53:12 -0500

    Randomized the color of each circle and added style to the button

---
+++ code.js
@@ -0,0 +1,34 @@
+const btn = document.querySelector('button');
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+let WIDTH = document.documentElement.clientWidth;
+let HEIGHT = document.documentElement.clientHeight;
+
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+
+function random(number) {
+    return Math.floor(Math.random() * number);
+}
+
+function draw() {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < 6; j++) {
+            for (let k = 0; k < 6; k++) {
+                ctx.beginPath();
+                ctx.fillStyle = `rgba(
+                ${Math.floor(255 - 42.5 * i)},
+                ${Math.floor(255 - 42.5 * j)},
+                ${Math.floor(255 - 42.5 * k)},
+                ${Math.random()})`;
+                ctx.arc(random(WIDTH), random(HEIGHT), random(50), 0, 2 * Math.PI);
+                ctx.fill();
+            }
+            
+        }
+    }
+}
+
+btn.addEventListener('click', draw);
